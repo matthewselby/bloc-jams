9bc3cbc5dd7c7786a3a302d6ffbd6c109df06d3d
Matthew Selby <matthewcselby@icloud.com>
2017-10-01 10:40:39 -0700

    cp9 work - adding animation on scroll

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() { 
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName ('point');
 
+var animatePoints = function(points) { 
   var revealPoint = function(index) {
     points[index].style.opacity = 1;
     points[index].style.transform = "scaleX(1) translateY(0)";
@@ -12,3 +12,18 @@ var animatePoints = function() {
       revealPoint(i);
   }
 };
+
+window.onload = function() {
+  // animate if screen is large enough to display all points
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  // getting the distance between the sellingPoints section and the top of the page + 200px
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener("scroll", function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  })
+}
